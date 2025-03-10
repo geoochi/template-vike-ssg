c96dcbd4ade189cfbd1337a0304da5a29966c914
Geoo Chi <geoochi@geoochi.com>
2025-03-10 21:39:11 +0800

    update

--- src/pages/_error/+Page.tsx
+++ src/pages/_error/+Page.tsx
@@ -1,9 +1,11 @@
 import { usePageContext } from 'vike-react/usePageContext'
 
-export default function Page() {
+const Page: React.FC = () => {
   const { is404 } = usePageContext()
   if (is404) {
     return <h1 className='text-4xl'>404 Page Not Found</h1>
   }
   return <h1 className='text-4xl'>500 Internal Server Error</h1>
 }
+
+export default Page
